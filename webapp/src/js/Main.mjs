beadbe977c00788be2c103911b2866e9bd428c32
noahmerg <nhmr2550@hochschule-trier.de>
2023-12-17 15:22:55 +0100

    imported used files for esbuild

--- webapp/src/js/Main.mjs
+++ webapp/src/js/Main.mjs
@@ -1,6 +1,11 @@
-const c = 3;
-if (c === 3) {
-  console.log(3);
-}
+
 // esbuild gonna put this stuff into "bundle.js"
 // & terser gonna make it only the console.log line, since rest is useless
+
+import { handleVisibilityChaange } from "./attention-seeker.mjs";
+import { initializeFilterDropdown } from './nextsite/filter-dropdown.mjs';
+
+window.onload = function () {
+  handleVisibilityChaange();
+  initializeFilterDropdown();
+}
