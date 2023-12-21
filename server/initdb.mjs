18f689c3f493fbc1f980eebc07c6d6252f38c1b9
noahmerg <nhmr2550@hochschule-trier.de>
2023-12-22 00:54:09 +0100

    implemented example code (works)

--- server/initdb.mjs
+++ server/initdb.mjs
@@ -1,1 +1,28 @@
-console.log('I AM FILLING THE DATABASEEE');
+// uri for database: mongodb+srv://noahmerg:<password>@name-voyager.jgcwhtt.mongodb.net/?retryWrites=true&w=majority
+
+import config from './config.json' assert {type: 'json'};
+const { username, password } = config;
+import { MongoClient } from 'mongodb';
+
+// MongoDB Atlas connection string
+const uri = `mongodb+srv://${username}:${password}@name-voyager.jgcwhtt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+// Function to populate the database
+async function populateDatabase() {
+  try {
+    // Connect to MongoDB Atlas
+    await client.connect();
+    console.log('Connected to MongoDB Atlas');
+
+    // Continue with the rest of your script...
+
+  } finally {
+    // Close the MongoDB Atlas connection
+    await client.close();
+    console.log('Disconnected from MongoDB Atlas');
+  }
+}
+
+// Call the function to populate the database
+populateDatabase();
