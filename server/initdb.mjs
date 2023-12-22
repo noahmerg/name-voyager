1fbfea1ce3f45bef050875560e75bc77fb2ca7c0
noahmerg <nhmr2550@hochschule-trier.de>
2023-12-23 00:17:47 +0100

    localhost test

--- server/initdb.mjs
+++ server/initdb.mjs
@@ -1,12 +1,22 @@
 // uri for database: mongodb+srv://noahmerg:<password>@name-voyager.jgcwhtt.mongodb.net/?retryWrites=true&w=majority
 
 import config from './config.json' assert {type: 'json'};
-const { username, password } = config;
 import { MongoClient } from 'mongodb';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
 
-// MongoDB Atlas connection string
-const uri = `mongodb+srv://${username}:${password}@name-voyager.jgcwhtt.mongodb.net/?retryWrites=true&w=majority`;
+const { username, password } = config;
+// const uri = `mongodb.localhost:22017... wenn selbst gehostet
+const uri = 'mongodb://localhost:27017/name-voyager.names';
 const client = new MongoClient(uri);
+
+const databaseName = 'name-voyager';
+const collectionName = 'names';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Function to populate the database
 async function populateDatabase() {
@@ -14,6 +24,27 @@ async function populateDatabase() {
     // Connect to MongoDB Atlas
     await client.connect();
     console.log('Connected to MongoDB Atlas');
+
+    const database = client.db(databaseName);
+    const dataCollection = database.collection(collectionName);
+
+    const csvPath = `${__dirname}/Namen.csv`;
+    const csvData = readFileSync(csvPath, 'utf8');
+
+    const rows = csvData.trim().split('\n').map(row => row.split(';'));
+
+    const headers = rows.shift();
+
+    const data = rows.map(row => {
+      const obj = {};
+      headers.forEach((header, i) => {
+        obj[header] = row[i];
+      });
+      return obj;
+    });
+
+    // Insert data into MongoDB collection
+    await dataCollection.insertMany(data);
 
     // Continue with the rest of your script...
 
