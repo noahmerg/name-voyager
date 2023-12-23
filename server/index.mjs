48fb51b8dcd05a6bdda12bb24c5a019f0dbd732c
Philipp Le <phx4759@hochschule-trier>
2023-12-23 19:13:37 +0100

    automatically reload browser after changes for developement pupurposes

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,5 @@
 import path from 'path';
-
+import reload from 'reload';
 import express from 'express';
 
 // use second argument or 8080 as PORT for server
@@ -12,4 +12,4 @@ const server = express();
 server.use(express.static(path.join(path.dirname(process.argv[1]), '../webapp/dist')));
 
 // listen on PORT
-server.listen(PORT, () => { console.log('HTTP server listening on port %d.', PORT); });
+reload(server).then(function (reloadReturned) { server.listen(PORT, () => { console.log('HTTP server listening on port %d.', PORT); }); }).catch(function (err) { console.error('Reload could not start, could not start server', err); });
