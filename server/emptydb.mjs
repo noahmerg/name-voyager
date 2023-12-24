aa94dd242d84f3abbda80bba3e54821f71b27c4d
noahmerg <nhmr2550@hochschule-trier.de>
2023-12-24 13:37:36 +0100

    code cleanup

--- server/emptydb.mjs
+++ server/emptydb.mjs
@@ -1,4 +1,5 @@
 import { MongoClient } from 'mongodb';
+import { connectToDatabase, closeDatabaseConnection } from './db.mjs';
 
 // TO RUN A MONGO INSTANCE: sudo mongod --dbpath ~/data/db
 const uri = 'mongodb://localhost:27017';
@@ -9,20 +10,19 @@ const collectionName = 'names';
 
 async function emptyDatabase() {
   try {
-    await client.connect();
-    console.log('Connected to MongoDB Atlas');
+    await connectToDatabase();
 
     const database = client.db(databaseName);
     const dataCollection = database.collection(collectionName);
   
     await dataCollection.deleteMany();
-
+    console.log("Deleted all data from database");
   } catch(error){
     console.error(error);
   } finally {
-    await client.close();
-    console.log('Disconnected from MongoDB Atlas');
+    await closeDatabaseConnection();
+    process.exit(0);
   }
 }
   
-emptyDatabase();
+await emptyDatabase();
