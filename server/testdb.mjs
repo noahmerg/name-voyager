8b87c6bdc33cd172ed74f6104455e2a9b3b83f16
noahmerg <n.mergarten@gmail.com>
2023-12-31 04:24:23 +0100

    prints random name ( + syllables) now

--- server/testdb.mjs
+++ server/testdb.mjs
@@ -18,8 +18,11 @@ async function testdb () {
     if (names.length === 0) throw MongoError;
 
     console.log('Number of Names found: %s', names.length);
-
-    console.log('First Name found: %s. Gender: %s', names[0].vorname, names[0].geschlecht);
+    console.log('Random name as an example that DB was filles correctly: ');
+    const rand = Math.floor(Math.random() * names.length);
+    console.log('Vorname: ' + names[rand].name);
+    console.log('Geschlecht: ' + names[rand].gender);
+    console.log('Silben: ' + names[rand].syllables);
   } catch (error) {
     console.error('something in the DB went wrong: ' + error);
   } finally {
