e72ba25af78a5ca67dba5466f9c8423038e8e7fa
Philipp Le <phx4759@hochschule-trier>
2024-01-02 23:42:40 +0100

    minor visual changes to frontpage and bookmarklist

--- webapp/src/js/Main.mjs
+++ webapp/src/js/Main.mjs
@@ -2,11 +2,9 @@
 // esbuild gonna put this stuff into "bundle.js"
 
 import { handleVisibilityChange } from './attention-seeker.mjs';
-import { bookmarkPopup, bookmarkList } from './frontpage/bookmark.mjs';
 import { initializeSlider } from './nextsite/slider.mjs';
 import { searchListener } from './nextsite/search.mjs';
 import { resetListener } from './nextsite/reset.mjs';
-import { initializeFilterDropdown } from './nextsite/filter-dropdown.mjs';
 import { bookmarkPopup, bookmarkList, copyToClipboard, removeElement } from './bookmark.mjs';
 
 window.onload = function () {
