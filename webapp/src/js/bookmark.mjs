7a71cd4fd5f5d781ff1001be1d28aeffa08046f1
Philipp Le <phx4759@hochschule-trier>
2024-01-06 22:42:23 +0100

    comments

--- webapp/src/js/bookmark.mjs
+++ webapp/src/js/bookmark.mjs
@@ -1,15 +1,15 @@
+/**
+ * changes text of save button and opens/closes bookmarklist
+ */
 export function bookmarkPopup () {
   const bookmarkList = document.getElementById('bookmark-list');
   const button = document.getElementById('save-button');
   const buttonText = button.children[0].children[0];
-  // const buttonSymbol = button.children[0].children[1];
   button.addEventListener('click', function () {
     if (bookmarkList.classList.contains('open')) {
       bookmarkList.classList.remove('open');
       bookmarkList.classList.add('notOpen');
       buttonText.innerHTML = 'Bookmarks';
-      // beim öffnen der Bookliste hier eine Funktion machen, die eine Anfrage erstellt und dann die
-      // jeweiligen Elemente erstellt
     } else {
       bookmarkList.classList.remove('notOpen');
       bookmarkList.classList.add('open');
@@ -17,14 +17,18 @@ export function bookmarkPopup () {
     }
   });
 }
-
+/**
+ * adds respective event listener to bookmarklist for functionality
+ */
 export function bookmarkList () {
   const bookmarkList = document.querySelector('.bookmark-body');
   const toggleButtons = document.querySelectorAll('input[name="gender-toggle"]');
-  // const orderedList = [...bookmarkList.children].map((element, index)=>{return {element:element,index:index}});
   let draggedItem = null;
   let requestGender = 'both';
   const button = document.getElementById('save-button');
+  /**
+   * creates favourite names list elements and fill them into bookmarkbody by pulling the names from the database
+   */
   async function fillBody () {
     try {
       let response = null;
@@ -38,50 +42,72 @@ export function bookmarkList () {
       result.forEach((value) => {
         const exists = Array.from(bookmarkList.getElementsByClassName('favname')).some(el => el.textContent.includes(value.name));
         if (!exists) {
-          console.log('penis');
-          const element = createFavNameElement(value);
-          const text = createNameText(value);
-          const iconContainer = createIconContainer();
-          const dragIconPath = './assets/drag_handle.svg';
-          const copyIconPath = './assets/content_copy.svg';
-          const deleteIconPath = './assets/delete.svg';
-          if (requestGender === 'both') {
-            const dragIcon = createDragIcon(dragIconPath);
-            iconContainer.appendChild(dragIcon);
-          }
-          const copyIcon = createCopyIcon(copyIconPath);
-          const deleteIcon = createDeleteIcon(deleteIconPath);
-          element.appendChild(text);
-          element.appendChild(iconContainer);
-          iconContainer.appendChild(copyIcon);
-          iconContainer.appendChild(deleteIcon);
-          bookmarkList.appendChild(element);
+          createFavName(value);
         }
       });
     } catch (error) {
       console.log(error);
     }
-
+    /**
+     * creates a fav name element for the list and appends it to the list
+     * @param {JSON} value needed to pull name and index from it
+     */
+    function createFavName (value) {
+      console.log('penis');
+      const element = createFavNameContainer(value);
+      const text = createNameText(value);
+      const iconContainer = createIconContainer();
+      const dragIconPath = './assets/drag_handle.svg';
+      const copyIconPath = './assets/content_copy.svg';
+      const deleteIconPath = './assets/delete.svg';
+      if (requestGender === 'both') {
+        const dragIcon = createDragIcon(dragIconPath);
+        iconContainer.appendChild(dragIcon);
+      }
+      const copyIcon = createCopyIcon(copyIconPath);
+      const deleteIcon = createDeleteIcon(deleteIconPath);
+      element.appendChild(text);
+      element.appendChild(iconContainer);
+      iconContainer.appendChild(copyIcon);
+      iconContainer.appendChild(deleteIcon);
+      bookmarkList.appendChild(element);
+    }
+    /**
+     * creates Icon Container Div
+     * @returns {HTMLDivElement} icon container
+     */
     function createIconContainer () {
       const iconContainer = document.createElement('div');
       iconContainer.classList.add('icon-container');
       return iconContainer;
     }
-
+    /**
+     * creates a span with the favourite name by pulling it from the json
+     * @param {JSON} value
+     * @returns {HTMLSpanElement} span with name
+     */
     function createNameText (value) {
       const text = document.createElement('span');
       text.textContent = value.name;
       return text;
     }
-
-    function createFavNameElement (value) {
+    /**
+     * creates a div with the class favname that is draggable and assigns a index extracting it from the JSON to it
+     * @param {JSON} value
+     * @returns {HTMLDivElement} returns favname div
+     */
+    function createFavNameContainer (value) {
       const element = document.createElement('div');
       element.setAttribute('draggable', 'true');
       element.classList.add('favname');
       element.setAttribute('data-index', `${value.index}`);
       return element;
     }
-
+    /**
+     * creates a delete icon and assigns all needed classes, attributes and eventlistener
+     * @param {String} deleteIconPath
+     * @returns {HTMLImageElement} returns delete icon
+     */
     function createDeleteIcon (deleteIconPath) {
       const deleteIcon = document.createElement('img');
       deleteIcon.setAttribute('src', deleteIconPath);
@@ -91,7 +117,11 @@ export function bookmarkList () {
       deleteIcon.addEventListener('click', removeFunction);
       return deleteIcon;
     }
-
+    /**
+     * creates a drag icon and assigns all needed classes and attributes
+     * @param {String} dragIconPath
+     * @returns {HTMLImageElement} returns drag icon
+     */
     function createDragIcon (dragIconPath) {
       const dragIcon = document.createElement('img');
       dragIcon.setAttribute('src', dragIconPath);
@@ -100,7 +130,11 @@ export function bookmarkList () {
       dragIcon.classList.add('drag-icon');
       return dragIcon;
     }
-
+    /**
+     * creates a copy icon and assigns all needed classes, attributes and eventlistener
+     * @param {String} copyIconPath
+     * @returns {HTMLImageElement} returns copy icon
+     */
     function createCopyIcon (copyIconPath) {
       const copyIcon = document.createElement('img');
       copyIcon.setAttribute('src', copyIconPath);
@@ -111,6 +145,9 @@ export function bookmarkList () {
       return copyIcon;
     }
   }
+  /**
+   * updates every data-index and index in the database of the respective element
+   */
   function updateIndices () {
     const items = document.querySelectorAll('.favname');
     items.forEach((item, index) => {
@@ -118,6 +155,19 @@ export function bookmarkList () {
       updateIndexInDatabase(item, index + 1);
     });
   }
+  /**
+   * removes every element in bookmarklist
+   */
+  function clearBody () {
+    while (bookmarkList.firstChild) {
+      bookmarkList.removeChild(bookmarkList.firstChild);
+    }
+  }
+  /**
+   * updates index of item in database
+   * @param {HTMLDivElement} item
+   * @param {Number} index
+   */
   async function updateIndexInDatabase (item, index) {
     try {
       const name = item.firstElementChild.textContent;
@@ -136,11 +186,20 @@ export function bookmarkList () {
       console.error('Error:', error);
     }
   }
-  function clearBody () {
-    while (bookmarkList.firstChild) {
-      bookmarkList.removeChild(bookmarkList.firstChild);
-    }
+  function copyFunction (event) {
+    const textElement = event.target.parentElement.parentElement.firstElementChild;
+    console.log(textElement);
+    navigator.clipboard.writeText(textElement.innerText);
   }
+  function removeFunction (event) {
+    const element = event.target.parentElement.parentElement;
+    element.remove();
+  }
+  /**
+   * selects dragged Item and adds dragging as class, allows dragging functionality
+   * if requested gender is at 'both' to not mess up indexes
+   * @param {DragEvent} event
+   */
   function dragstartHandler (event) {
     if (requestGender === 'both') {
       draggedItem = event.target;
@@ -149,6 +208,10 @@ export function bookmarkList () {
       event.preventDefault();
     }
   }
+  /**
+   *
+   * @param {DragEvent} event
+   */
   function dragoverHandler (event) {
     event.preventDefault();
     const dropzoneElement = determineDropPosition(event.clientY);
@@ -158,6 +221,11 @@ export function bookmarkList () {
       bookmarkList.insertBefore(draggedItem, dropzoneElement);
     }
   }
+  /**
+   *
+   * @param {Number} coordy
+   * @returns
+   */
   function determineDropPosition (coordy) {
     const listElements = [...document.querySelectorAll('.favname:not(.dragging)')];
     return listElements.reduce((cloestItem, listElement) => {
@@ -170,6 +238,10 @@ export function bookmarkList () {
       }
     }, { offset: Number.NEGATIVE_INFINITY }).element;
   }
+  /**
+   *
+   * @param {dragEndHandler} event
+   */
   function dragEndHandler (event) {
     draggedItem.classList.remove('dragging');
     updateIndices();
@@ -189,15 +261,6 @@ export function bookmarkList () {
   bookmarkList.addEventListener('dragstart', dragstartHandler);
   bookmarkList.addEventListener('dragover', dragoverHandler);
   bookmarkList.addEventListener('dragend', dragEndHandler);
-}
-function copyFunction (event) {
-  const textElement = event.target.parentElement.parentElement.firstElementChild;
-  console.log(textElement);
-  navigator.clipboard.writeText(textElement.innerText);
-}
-function removeFunction (event) {
-  const element = event.target.parentElement.parentElement;
-  element.remove();
 }
 export function saveName () {
   const saveButtons = document.getElementsByClassName('save-name-button');
