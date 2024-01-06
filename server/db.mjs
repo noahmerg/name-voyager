4b376af5ecff5447b26f012093e264d2dffe6d78
noahmerg <n.mergarten@gmail.com>
2024-01-06 21:49:13 +0100

    testdb.mjs fully relies on db.mjs connection now

--- server/db.mjs
+++ server/db.mjs
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb';
 
 const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri);
+export const namesCollection = client.db('name-voyager').collection('names');
 
 async function connectToDatabase () {
   await client.connect();
--- server/testdb.mjs
+++ server/testdb.mjs
@@ -1,19 +1,11 @@
-import { MongoClient, MongoError } from 'mongodb';
-import { connectToDatabase, closeDatabaseConnection } from './db.mjs';
-
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
-const databaseName = 'name-voyager';
-const collectionName = 'names';
+import { MongoError } from 'mongodb';
+import { connectToDatabase, closeDatabaseConnection, namesCollection } from './db.mjs';
 
 async function testdb () {
   try {
     await connectToDatabase();
 
-    const database = client.db(databaseName);
-    const collection = database.collection(collectionName);
-
-    const names = await collection.find().toArray();
+    const names = await namesCollection.find().toArray();
 
     if (names.length === 0) throw MongoError;
 
