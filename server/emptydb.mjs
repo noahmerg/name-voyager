a7106da11dc7fbb102296a7d0602a927579fed5a
noahmerg <n.mergarten@gmail.com>
2024-01-06 21:58:11 +0100

    emptydb.mjs fully relies on db.mjs connection now

--- server/emptydb.mjs
+++ server/emptydb.mjs
@@ -1,24 +1,10 @@
-import { MongoClient } from 'mongodb';
-import { connectToDatabase, closeDatabaseConnection } from './db.mjs';
-
-// TO RUN A MONGO INSTANCE: sudo mongod --dbpath ~/data/db
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
-
-const databaseName = 'name-voyager';
-const namesCollectionName = 'names';
-const bookmarkCollectionName = 'bookmarklist';
+import { connectToDatabase, closeDatabaseConnection, namesCollection, bookmarkCollection } from './db.mjs';
 
 async function emptyDatabase () {
   try {
     await connectToDatabase();
-
-    const database = client.db(databaseName);
-    const bookmarkCollection = database.collection(bookmarkCollectionName);
-    const dataCollection = database.collection(namesCollectionName);
-
     await bookmarkCollection.deleteMany();
-    await dataCollection.deleteMany();
+    await namesCollection.deleteMany();
     console.log('Deleted all data from database');
   } catch (error) {
     console.error(error);
