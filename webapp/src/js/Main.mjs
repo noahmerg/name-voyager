0d29c702c53001241eedf7fb0d0bd191b0c9c608
Philipp Le <phx4759@hochschule-trier>
2024-01-06 17:55:58 +0100

    fix bookmarklist listeners for icons

--- webapp/src/js/Main.mjs
+++ webapp/src/js/Main.mjs
@@ -5,7 +5,7 @@ import { handleVisibilityChange } from './attention-seeker.mjs';
 import { initializeSlider } from './nextsite/slider.mjs';
 import { searchListener } from './nextsite/search.mjs';
 import { resetListener } from './nextsite/reset.mjs';
-import { bookmarkPopup, bookmarkList, copyToClipboard, removeElement } from './bookmark.mjs';
+import { bookmarkPopup, bookmarkList } from './bookmark.mjs';
 
 import { starMover } from './stars.mjs';
 const images = document.querySelectorAll('.star-images');
@@ -17,8 +17,6 @@ window.onload = function () {
   initializeSlider();
   searchListener();
   resetListener();
-  copyToClipboard();
-  removeElement();
   starMover();
   images.forEach(function (img) {
     img.addEventListener('contextmenu', function (e) {
